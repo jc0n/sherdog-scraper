2ef59f82cc48647fae77d2ebbb40d8f093bb27e3
John O'Connor <tehjcon@gmail.com>
2012-07-21 00:10:21 -0400

    Finish draft of API + basic tests

--- sherdog.py
+++ sherdog.py
@@ -5,11 +5,12 @@ and events from Sherdog.com
 
 import abc
 import json
+import math
 import re
 import urllib
 
 from collections import namedtuple
-from datetime import timedelta
+from datetime import timedelta, datetime
 from weakref import WeakValueDictionary
 
 # dependencies
@@ -17,6 +18,7 @@ import iso8601
 from BeautifulSoup import BeautifulSoup
 
 __author__ = 'John O\'Connor'
+__all__ = ('Sherdog', 'Event', 'Fight', 'Fighter', 'Organization')
 
 SHERDOG_URL = 'http://www.sherdog.com'
 
@@ -27,36 +29,44 @@ _FIGHTER_RIGHT_RE = re.compile('fighter right_side')
 _FINAL_RESULT_RE = re.compile('^final_result')
 _SUB_EVENT_RE = re.compile('subEvent')
 
-__all__ = ('Sherdog', )
+_EVENTS_URL_RE = re.compile('^/events/')
+_FIGHTER_URL_RE = re.compile('^/fighter/')
+
+_SECONDS_IN_YEAR = 60 * 60 * 24 * 365
 
 class LazySherdogObject(object):
     __metaclass__ = abc.ABCMeta
     _lazy = True
     _url_path = abc.abstractproperty()
 
-    def __init__(self, sherdog, id_or_url=None, **kwargs):
-        self._sherdog = sherdog
-        if id_or_url is not None:
-            if isinstance(id_or_url, basestring):
-                self.url = id_or_url
-                self.id = int(self.url[self.url.rfind('-') + 1:])
-            else:
-                self.id = int(id_or_url)
-                self.url = self._url_path % self.id
-
+    def __init__(self, id_or_url, **kwargs):
         for key, value in kwargs.iteritems():
             setattr(self, key, value)
+
+        if isinstance(id_or_url, basestring):
+            self.id = int(id_or_url[id_or_url.rfind('-') + 1:])
+        else:
+            self.id = int(id_or_url)
+
+        self.url = self._url_path % self.id
 
     def __getattr__(self, key):
         if not self._lazy:
             raise AttributeError(key)
 
-        self._load_properties()
         self._lazy = False
+        self._load_properties()
         return getattr(self, key)
 
     def __getitem__(self, key):
         return getattr(self, key)
+
+    def __eq__(self, other):
+        assert isinstance(other, LazySherdogObject)
+        return self.id == other.id
+
+    def __hash__(self):
+        return hash(self.id)
 
     @abc.abstractmethod
     def _load_properties(self):
@@ -68,9 +78,13 @@ class Organization(LazySherdogObject):
     _search_url_path = '/search/organizations/?q=%s'
 
     def _load_properties(self):
-        dom = self._sherdog.fetch_and_parse_url(self.url)
+        dom = Sherdog.fetch_and_parse_url(self.url)
+
+        name = dom.find('h2', {'itemprop': 'name'})
+        self.name = name.text if name else None
+
         description = dom.find('div', {'class': 'data', 'itemprop': 'description'})
-        self.description = description.text
+        self.description = description.text if description else None
 
         self.events = []
         table = dom.find('table', {'class': 'event'})
@@ -80,18 +94,15 @@ class Organization(LazySherdogObject):
             date = iso8601.parse_date(datestr)
             name = row.find('span', {'itemprop': 'name'}).text
             location = row.find('td', {'itemprop': 'location'}).text
-            self.events.append(Event(self._sherdog, url=row.a['href'], date=date,
-                    location=location, name=name))
-
+            event = Event(row.a['href'], date=date, location=location, name=name)
+            self.events.append(event)
 
     @classmethod
-    def search(cls, query, sherdog=None):
-        if sherdog is None:
-            sherdog = Sherdog()
+    def search(cls, query):
         query = urllib.quote(query.lower())
-        result = sherdog.fetch_url(self._search_url_path % query)
+        result = Sherdog.fetch_url(cls._search_url_path % query)
         data = json.loads(result)
-        return [Organization(self, **orgdict) for orgdict in data['collection']]
+        return [Organization(id_or_url=orgdict['id'], **orgdict) for orgdict in data['collection']]
 
     def __repr__(self):
         return repr(self.name)
@@ -99,17 +110,83 @@ class Organization(LazySherdogObject):
 
 class Fighter(LazySherdogObject):
     _url_path = '/fighter/X-%d'
+    _search_url_path = '/stats/fightfinder?SearchTxt=%s'
 
     def __repr__(self):
         return repr(self.name)
 
     @property
+    def age(self):
+        seconds = (datetime.now() - self.birthday).total_seconds()
+        return int(math.floor(seconds / _SECONDS_IN_YEAR))
+
+    @property
     def full_url(self):
         return SHERDOG_URL + self.url
 
+    @classmethod
+    def search(cls, query):
+        query = urllib.quote(query.lower())
+        dom = Sherdog.fetch_and_parse_url(cls._search_url_path % query)
+        table = dom.find('table', {'class': 'fightfinder_result'})
+        urls = [a['href'] for a in table.findAll('a')]
+        return map(cls, filter(_FIGHTER_URL_RE.match, urls))
+
     def _load_properties(self):
-        # TODO
-        pass
+        dom = Sherdog.fetch_and_parse_url(self.url)
+
+        image = dom.find('img', {'class': 'profile_image photo', 'itemprop': 'image'})
+        self.image_url = image['src'] if image else None
+
+        name = dom.find('span', {'class': 'fn'})
+        self.name = name.text if name else None
+
+        nickname = dom.find('span', {'class': 'nickname'})
+        self.nickname = nickname.text.strip('\'"') if nickname else None
+
+        birthday = dom.find('span', {'itemprop': 'birthDate'})
+        if birthday and '-' in birthday.text:
+            self.birthday = datetime.strptime(birthday.text, "%Y-%m-%d").date()
+        else:
+            self.birthday = None
+
+        birthplace = dom.find('span', {'class': 'item birthplace'})
+        if birthplace:
+            city = birthplace.find('span', {'class': 'locality'})
+            self.city = city.text if city else None
+            country = birthplace.find('strong', {'itemprop': 'nationality'})
+            self.country = country.text if country else None
+            self.country_flag_url = birthplace.img['src'] if birthplace.img else None
+        else:
+            self.city = self.country = self.country_flag_url = None
+
+        height = dom.find('span', {'class': 'item height'})
+        self.height = height.strong.text if height and hasattr(height, 'strong') else None
+
+        weight = dom.find('span', {'class': 'item weight'})
+        self.weight = weight.strong.text if weight and hasattr(weight, 'strong') else None
+
+        wclass = dom.find('h6', {'class': 'item wclass'})
+        self.weight_class = wclass.strong.text if wclass and hasattr(wclass, 'strong') else None
+
+        win_graph = dom.find('div', {'class': 'bio_graph'})
+        if win_graph:
+            counter = win_graph.find('span', {'class': 'counter'})
+            self.wins = int(counter.text) if counter else 0
+        else:
+            self.wins = None
+
+        lose_graph = dom.find('div', {'class': 'bio_graph loser'})
+        if lose_graph:
+            counter = lose_graph.find('span', {'class': 'counter'})
+            self.losses = int(counter.text) if counter else 0
+        else:
+            self.losses = None
+
+        content = dom.find('div', {'class': 'content table'})
+        if content and hasattr(content, 'table'):
+            event_links = content.table.findAll('a', {'href': _EVENTS_URL_RE})
+            self.events = [Event(a['href']) for a in event_links]
 
 
 class Fight(namedtuple('Fight', ('event', 'fighters', 'match',
@@ -117,7 +194,8 @@ class Fight(namedtuple('Fight', ('event', 'fighters', 'match',
                                  'time', 'winner'))):
 
     def __repr__(self):
-        return u' vs. '.join([f.name for f in self.fighters])
+        return u' vs. '.join([f.name.split(None, 1)[-1].title()
+                                for f in self.fighters])
 
 
 class Event(LazySherdogObject):
@@ -133,7 +211,7 @@ class Event(LazySherdogObject):
         return repr(self.name)
 
     def _parse_fight_time(self, timestr):
-        if not timestr:
+        if not timestr or ':' not in timestr:
             return None
         minutes, seconds = timestr.split(':', 1)
         return timedelta(minutes=int(minutes), seconds=int(seconds))
@@ -148,10 +226,10 @@ class Event(LazySherdogObject):
 
     def _parse_main_fight(self, dom):
         left = dom.find('div', {'class': _FIGHTER_LEFT_RE}).h3.a
-        left_fighter = Fighter(self._sherdog, left['href'], name=left.text)
+        left_fighter = Fighter(left['href'], name=left.text)
 
         right = dom.find('div', {'class': _FIGHTER_RIGHT_RE}).h3.a
-        right_fighter = Fighter(self._sherdog, right['href'], name=right.text)
+        right_fighter = Fighter(right['href'], name=right.text)
 
         fighters = (left_fighter, right_fighter)
         result = dom.find('span', {'class': _FINAL_RESULT_RE})
@@ -168,14 +246,14 @@ class Event(LazySherdogObject):
         time = self._parse_fight_time(info['time'])
         return Fight(event=self, fighters=fighters,
                 winner=self._fight_winner(result, left_fighter, right_fighter),
-                match=info['match'], method=info['method'], referee=info['referee'],
-                round=info['round'], time=time)
+                match=int(info['match']), method=info['method'], referee=info['referee'],
+                round=int(info['round']), time=time)
 
     def _parse_sub_fight(self, row):
         td = row.findAll('td')
         left, right = td[1].a, td[3].a
-        left_fighter = Fighter(self._sherdog, left['href'], name=left.text)
-        right_fighter = Fighter(self._sherdog, right['href'], name=right.text)
+        left_fighter = Fighter(left['href'], name=left.text)
+        right_fighter = Fighter(right['href'], name=right.text)
         fighters = (left_fighter, right_fighter)
         result = td[1].find('span', {'class': _FINAL_RESULT_RE})
         if result is None:
@@ -186,9 +264,9 @@ class Event(LazySherdogObject):
         return Fight(event=self,
                      fighters=fighters,
                      winner=self._fight_winner(result, left_fighter, right_fighter),
-                     match=td[0].text,
+                     match=int(td[0].text),
                      method=td[4].contents[0],
-                     referee=td[4].contents[-1],
+                     referee=td[4].contents[-1].text,
                      round=int(td[5].text),
                      time=self._parse_fight_time(td[6].text))
 
@@ -198,37 +276,35 @@ class Event(LazySherdogObject):
         return [self._parse_sub_fight(row) for row in rows]
 
     def _load_properties(self):
-        dom = self._sherdog.fetch_and_parse_url(self.url)
+        dom = Sherdog.fetch_and_parse_url(self.url)
         detail = dom.find('div', {'class': 'event_detail'})
         self.name = detail.span.text
         datestr = detail.find('meta', {'itemprop': 'startDate'})['content']
-        self.date = iso8601.parse_date(datestr)
+        self.date = iso8601.parse_date(datestr).date()
         location = detail.find('span', {'itemprop': 'location'}).text
         venue, location = location.split(',', 1)
-        self.location = location
+        self.location = location.lstrip()
         self.location_thumb_url = detail.find('span', {'class': 'author'}).img['src']
         self.venue = venue.lstrip()
         org = dom.find('div', {'itemprop': 'attendee'})
-        self.organization = Organization(self._sherdog, org.a['href'], name=org.span.text)
+        self.organization = Organization(org.a['href'], name=org.span.text)
         self.fights = [self._parse_main_fight(dom)]
         self.fights.extend(self._parse_sub_fights(dom))
+        self.fights.reverse()
 
     @classmethod
-    def search(cls, query, sherdog=None):
-        if sherdog is None:
-            sherdog = Sherdog()
+    def search(cls, query):
         query = urllib.quote(query.lower())
-        dom = sherdog.fetch_and_parse_url(self._search_url_path % query)
+        dom = Sherdog.fetch_and_parse_url(cls._search_url_path % query)
         table = dom.find('table', {'class': 'fightfinder_result'})
-        links = table.findAll('a')
-        return [cls(sherdog, a['href']) for a in links]
+        urls = [a['href'] for a in table.findAll('a')]
+        return map(cls, filter(_EVENTS_URL_RE.match, urls))
 
 
 class Sherdog(object):
-    def __init__(self):
-        self._cache = WeakValueDictionary()
 
-    def fetch_url(self, path):
+    @classmethod
+    def fetch_url(cls, path):
         assert path.startswith('/')
         url = SHERDOG_URL + path
         handle = urllib.urlopen(url)
@@ -236,32 +312,33 @@ class Sherdog(object):
         handle.close()
         return data
 
-    def fetch_and_parse_url(self, path):
+    @classmethod
+    def fetch_and_parse_url(cls, path):
         assert path.startswith('/')
-        soup = self._cache.get(path)
-        if not soup:
-            result = self.fetch_url(path)
-            soup = BeautifulSoup(result)
-            self._cache[path] = soup
+        result = cls.fetch_url(path)
+        soup = BeautifulSoup(result)
         return soup
 
-    def get_fighter(self, id_or_url):
-        return Fighter(self, id_or_url)
+    @classmethod
+    def get_fighter(cls, id_or_url):
+        return Fighter(id_or_url)
 
-    def get_event(self, id_or_url):
-        return Event(self, id_or_url)
+    @classmethod
+    def get_event(cls, id_or_url):
+        return Event(id_or_url)
 
-    def get_organization(self, id_or_url):
-        return Organization(self, id_or_url)
+    @classmethod
+    def get_organization(cls, id_or_url):
+        return Organization(id_or_url)
 
-    def search_events(self, query):
-        return Event.search(query, sherdog=self)
+    @classmethod
+    def search_events(cls, query):
+        return Event.search(query)
 
-    def search_organizations(self, query):
-        return Organization.search(query, sherdog=self)
+    @classmethod
+    def search_organizations(cls, query):
+        return Organization.search(query)
 
-
-import pdb
-s = Sherdog()
-print s.search_events('ufc 100')
-print s.search_events('ufc 149')
+    @classmethod
+    def search_fighters(cls, query):
+        return Fighter.search(query)
---
+++ tests/test_sherdog.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+from datetime import date, timedelta
+from unittest import TestCase, main
+
+from sherdog import Sherdog, Event, Organization, Fighter
+
+FRANK_MIR_ID = 2329
+FRANK_MIR_URL = '/fighter/Frank-Mir-2329'
+
+JUNIOR_DOS_SANTOS_ID = 17272
+
+UFC = Organization('Ultimate-Fighting-Championship-2')
+UFC_ID = 2
+
+UFC146 = Event('UFC-146-Dos-Santos-vs-Mir-20353')
+
+class TestSherdog(TestCase):
+
+    def test_get_fighter(self):
+        for f in map(Sherdog.get_fighter, (FRANK_MIR_ID, FRANK_MIR_URL)):
+            self.assertEquals(f.name, u'Frank Mir')
+            self.assertEquals(f.birthday, date(1979, 5, 24))
+            self.assertEquals(f.height, u'6\'3"')
+            weight = f.weight.split()
+            self.assertAlmostEquals(int(weight[0]), 260, delta=25)
+            self.assertEquals(weight[1], u'lbs')
+            self.assertEquals(f.city, u'Las Vegas, Nevada')
+            self.assertEquals(f.country, u'United States')
+            self.assertEquals(f.country_flag_url,
+                    u'http://www3.cdn.sherdog.com/1026/img/w/flags/us.png')
+            self.assertGreater(f.wins, 15)
+            self.assertGreater(f.losses, 5)
+            self.assertEquals(f.image_url,
+                    u'http://www1.cdn.sherdog.com/image_crop/200/300/_images/fighter/20110412122006_20091214122837_IMG_5197.JPG')
+
+            self.assertIn(UFC146, f.events)
+            self.assertGreater(len(f.events), 21)
+
+    def test_get_event(self):
+        event = Sherdog.get_event('UFC-146-Dos-Santos-vs-Mir-20353')
+        self.assertEquals(event.name, u'UFC 146 - Dos Santos vs. Mir')
+        self.assertEquals(event.organization, UFC)
+        self.assertEquals(event.date, date(2012, 5, 26))
+        self.assertEquals(event.venue, u'MGM Grand Garden Arena')
+        self.assertEquals(event.location, u'Las Vegas, United States')
+        self.assertEquals(len(event.fights), 12)
+
+        frank = Fighter(FRANK_MIR_ID)
+        junior = Fighter(JUNIOR_DOS_SANTOS_ID)
+        main_fight = event.fights[-1]
+        self.assertEquals(main_fight.fighters, (junior, frank))
+        self.assertEquals(main_fight.winner, junior)
+        self.assertEquals(main_fight.match, 12)
+        self.assertEquals(main_fight.method, u'TKO (Punches)')
+        self.assertEquals(main_fight.referee, u'Herb Dean')
+        self.assertEquals(main_fight.round, 2)
+        self.assertEquals(main_fight.time, timedelta(minutes=3, seconds=4))
+
+        cain = Fighter('Cain-Velasquez-19102')
+        bigfoot = Fighter('Antonio-Silva-12354')
+        other_fight = event.fights[-2]
+        self.assertEquals(other_fight.fighters, (cain, bigfoot))
+        self.assertEquals(other_fight.winner, cain)
+        self.assertEquals(other_fight.match, 11)
+        self.assertEquals(other_fight.method, u'TKO (Punches)')
+        self.assertEquals(other_fight.round, 1)
+        self.assertEquals(other_fight.time, timedelta(minutes=3, seconds=36))
+        self.assertEquals(other_fight.referee, u'Josh Rosenthal')
+
+    def test_get_organization(self):
+        org = Sherdog.get_organization(UFC_ID)
+        self.assertEquals(org.name, u'Ultimate Fighting Championship')
+        self.assertGreater(len(org.events), 170)
+        self.assertIn(UFC146, org.events)
+
+    def test_search_events(self):
+        results = Sherdog.search_events('ufc 146')
+        self.assertGreaterEqual(len(results), 1)
+        self.assertEquals(results[0], UFC146)
+
+    def test_search_organizations(self):
+        results = Sherdog.search_organizations('ultimate fighting championship')
+        self.assertGreaterEqual(len(results), 3)
+        self.assertEquals(results[0], UFC)
+
+        results = Sherdog.search_organizations('strikeforce')
+        self.assertGreaterEqual(len(results), 1)
+        self.assertEquals(results[0], Organization('Strikeforce-716'))
+
+    def test_search_fighters(self):
+        results = Sherdog.search_fighters('frank mir')
+        self.assertGreaterEqual(len(results), 1)
+        self.assertEqual(results[0], Fighter(FRANK_MIR_ID))
+
+        results = Sherdog.search_fighters('junior dos santos')
+        self.assertGreaterEqual(len(results), 1)
+        self.assertEqual(results[0], Fighter(JUNIOR_DOS_SANTOS_ID))
+
+
+if __name__ == '__main__':
+    unittest.main()
