5033ec7fbfb57721069f708a9355430f46a2febf
John O'Connor <tehjcon@gmail.com>
2012-07-21 00:28:11 -0400

    Add setup and requirements file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sherdog-scraper',
+      version='0.1',
+      description='A web scraper for sherdog.com',
+      author='John O\'Connor',
+      url='https://github.com/jc0n/sherdog-scraper',
+      py_modules=['sherdog'],
+)
